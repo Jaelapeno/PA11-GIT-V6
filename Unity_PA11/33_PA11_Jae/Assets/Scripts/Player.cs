77bc699b735e74f548afc5750125c2632e1d189b
Jaelapeno <jaeloh00@gmail.com>
2020-06-18 14:27:37 +0800

    Updated by Jae

---
+++ Unity_PA11/33_PA11_Jae/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float score;
+
+    public Text Txt_Score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4f, 4f), transform.position.z);
+
+        Txt_Score.text = "Score : " + score;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Obstacle")
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        if (other.tag == "score")
+        {
+            score++;
+        }
+    }
+}
---
+++ Unity_PA11/33_PA11_Jae/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject[] SpawnObject;
+    public GameObject ScoreBar;
+
+    private int RandCube;
+
+    float PositionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void SpawnObjects()
+    {
+        RandCube = Random.Range(0, SpawnObject.Length);
+
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        Instantiate(SpawnObject[RandCube], transform.position, transform.rotation);
+        Instantiate(ScoreBar, transform.position, transform.rotation);
+    }
+}
